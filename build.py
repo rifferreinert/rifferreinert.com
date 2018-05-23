851cfccb1602f8d7d69a84c08f524519e3d9fb2d
Ben Riffer-Reinert <ben@rifferreinert.com>
2018-05-22 20:50:36 -0500

    Lets use some pylint

--- build.py
+++ build.py
@@ -1,13 +1,28 @@
+"""Build and push our docker files"""
+
 import docker
 
-client = docker.from_env()
-df = open('docker/app/Dockerfile', 'rb')
-client.images.build(tag='rifferreinert/rifferreinert_website_app',
-                    path='.', dockerfile='docker/app/Dockerfile', nocache=True)
-df = open('docker/web/Dockerfile', 'rb')
-client.images.build(tag='rifferreinert/rifferreinert_website_server',
-                    path='.', dockerfile='docker/web/Dockerfile', nocache=True)
-print('Images Built')
-client.images.push('rifferreinert/rifferreinert_website_app')
-client.images.push('rifferreinert/rifferreinert_website_server')
-print('Images Pushed')
+CLIENT = docker.from_env()
+
+
+def build_images():
+    """Build and tag docker images"""
+
+    CLIENT.images.build(tag='rifferreinert/rifferreinert_website_app',
+                        path='.', dockerfile='docker/app/Dockerfile', nocache=True)
+    CLIENT.images.build(tag='rifferreinert/rifferreinert_website_server',
+                        path='.', dockerfile='docker/web/Dockerfile', nocache=True)
+    print('Images Built')
+
+
+def push_images():
+    """Push docker images"""
+
+    CLIENT.images.push('rifferreinert/rifferreinert_website_app')
+    CLIENT.images.push('rifferreinert/rifferreinert_website_server')
+    print('Images Pushed')
+
+
+if __name__ == '__main__':
+    build_images()
+    push_images()
